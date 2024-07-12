74626a6ee5e5fc805ffadab9ba89cba70341f946
jubinjacob03 <jr.jubin03@gmail.com>
2024-07-13 00:53:35 +0530

    updated solidity contracts

--- contracts/Migrations.sol
+++ contracts/Migrations.sol
@@ -5,6 +5,9 @@ contract Migrations {
   address public owner = msg.sender;
   uint public last_completed_migration;
 
+  event MigrationCompleted(uint completed);
+  event OwnershipTransferred(address indexed previousOwner, address indexed newOwner);
+
   modifier restricted() {
     require(
       msg.sender == owner,
@@ -15,5 +18,16 @@ contract Migrations {
 
   function setCompleted(uint completed) public restricted {
     last_completed_migration = completed;
+    emit MigrationCompleted(completed);
+  }
+
+  function getOwner() public view returns (address) {
+    return owner;
+  }
+
+  function transferOwnership(address newOwner) public restricted {
+    require(newOwner != address(0), "New owner is the zero address");
+    emit OwnershipTransferred(owner, newOwner);
+    owner = newOwner;
   }
 }
--- contracts/TodoList.sol
+++ contracts/TodoList.sol
@@ -19,12 +19,26 @@ contract TodoList {
         bool completed
     );
 
+    event TaskDeleted (
+        uint id
+    );
+
+    event TaskUpdated (
+        uint id,
+        string content
+    );
+
     mapping(address => mapping(uint => Task)) public tasks;
     mapping(address => uint) public tasksCount;
-
+    mapping(address => uint) public completedTasksCount;
 
     constructor() {
         createTask("Hello World!!!");
+    }
+
+    modifier taskExists(uint _id) {
+        require(_id < tasksCount[msg.sender], "Task does not exist");
+        _;
     }
 
     function createTask(string memory _content) public {
@@ -34,12 +48,39 @@ contract TodoList {
         tasksCount[msg.sender]++;
     }
 
-    function toggleCompleted(uint _id) public {
-        Task memory task = tasks[msg.sender][_id];
+    function toggleCompleted(uint _id) public taskExists(_id) {
+        Task storage task = tasks[msg.sender][_id];
         task.completed = !task.completed;
         tasks[msg.sender][_id] = task;
+        if (task.completed) {
+            completedTasksCount[msg.sender]++;
+        } else {
+            completedTasksCount[msg.sender]--;
+        }
         emit TaskToggled(_id, task.completed);
     }
 
+    function deleteTask(uint _id) public taskExists(_id) {
+        delete tasks[msg.sender][_id];
+        emit TaskDeleted(_id);
+    }
 
+    function updateTaskContent(uint _id, string memory _content) public taskExists(_id) {
+        Task storage task = tasks[msg.sender][_id];
+        task.content = _content;
+        emit TaskUpdated(_id, _content);
+    }
+
+    function getCompletedTasksCount() public view returns (uint) {
+        return completedTasksCount[msg.sender];
+    }
+
+    function getAllTasks() public view returns (Task[] memory) {
+        uint taskCount = tasksCount[msg.sender];
+        Task[] memory allTasks = new Task[](taskCount);
+        for (uint i = 0; i < taskCount; i++) {
+            allTasks[i] = tasks[msg.sender][i];
+        }
+        return allTasks;
+    }
 }
